0829737ba3ce6962543a116e0c33facbcb4e4056
Mukund <mk789699@gmail.com>
2019-12-27 19:58:40 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { Server2Component } from './server2/server2.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    Server2Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/server2/server2.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+@Component({
+  selector:'app-server2',
+  templateUrl:'./server2.component.html',
+  styleUrls:['./server2.component.css']
+})
+export class Server2Component{
+
+}
